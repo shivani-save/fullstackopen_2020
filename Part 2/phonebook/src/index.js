6a87faf4e1e8da05983e8a57af0126d88f8c08d2
shivani-save <shivani.save8@gmail.com>
2020-06-26 20:45:23 +0530

    Part 2.6

---
+++ Part 2/phonebook/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+const handleNewName = (event) => {
+  setNewName(event.target.value)
+}
+
+const addEntry = event => {
+  event.preventDefault();
+  const person = {
+    name: newName
+  }
+  setPersons(persons.concat(person))
+  console.log(persons)
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addEntry}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+      <div>
+        <ul>
+          {persons.map(person => <li>{person.name}</li>)}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
